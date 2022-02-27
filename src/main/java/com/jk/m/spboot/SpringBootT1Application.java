a9da0d2812adc31683516545b147f30e4664c650
speed0dev <speed0dev@gmail.com>
2022-02-27 20:05:56 +0900

    sample

---
+++ src/main/java/com/jk/m/spboot/SpringBootT1Application.java
@@ -0,0 +1,14 @@
+package com.jk.m.spboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootT1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootT1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/m/spboot/SpringBootT1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jk.m.spboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootT1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+
+	}
+
+}
